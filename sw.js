a61ad4363ca5456956cf993f1171339aff8c3812
Deploy <d@e.co>
2026-07-12 22:11:20 +0000

    Indicateurs de messages non lus: nav Maintenant/Discussion et cartes activites

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v9";
+const CACHE = "vacances-v10";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 self.addEventListener("fetch", (e) => {
