026e6305f9cd766f2e4dba770a62330e4f9b6425
Deploy <d@e.co>
2026-07-12 21:31:22 +0000

    Boite de nuit: libelle Ce qui vous attend demain et activite cliquable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v6";
+const CACHE = "vacances-v7";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 self.addEventListener("fetch", (e) => {
