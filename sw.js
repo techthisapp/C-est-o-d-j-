74ce8a8f43e378b5dae587930061e5417ff066ad
Deploy <d@e.co>
2026-07-12 22:23:04 +0000

    Membres et droits: participants dynamiques, telephone/email + appel/whatsapp/mail, niveaux de droits

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v11";
+const CACHE = "vacances-v12";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 self.addEventListener("fetch", (e) => {
