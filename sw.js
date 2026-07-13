d108ed0dc2404bb56c86b950944e645411b6ce4d
Deploy <d@e.co>
2026-07-13 04:43:08 +0000

    Activites paralleles visibles sur l-accueil (en parallele sous les cartes)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v14";
+const CACHE = "vacances-v15";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
