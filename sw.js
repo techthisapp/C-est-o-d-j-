5c3eb69fd5af9a2b097696f3d8316ca8011acfe5
Deploy <d@e.co>
2026-07-13 20:56:13 +0000

    Sondages: option Autoriser les commentaires a la creation, fil de commentaires sous le sondage, fusion en synchro

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v44";
+const CACHE = "vacances-v45";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
