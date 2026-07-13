f6b71af8817e43cffd80ebb91752716d0b8cc2eb
Deploy <d@e.co>
2026-07-13 21:01:13 +0000

    Mur photos: a l-ajout, choix de rattacher la photo a une activite (ou le mur seulement)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v45";
+const CACHE = "vacances-v46";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
