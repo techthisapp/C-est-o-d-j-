df1b6e4d1606355e3c2b96a0c787bb33effa617a
Deploy <d@e.co>
2026-07-13 20:39:25 +0000

    Photo du jour sur l-accueil: derniere photo mise en avant (auteur, reactions), ouvre le visualiseur

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v41";
+const CACHE = "vacances-v42";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
