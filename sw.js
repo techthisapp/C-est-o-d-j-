3938d6696520f2168f8ba9f9982988819a133946
Deploy <d@e.co>
2026-07-13 20:36:27 +0000

    Mur de photos: onglet Photos remplace Le groupe (tri par recence), groupe deplace en bouton d-en-tete, section photos retiree du groupe

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v40";
+const CACHE = "vacances-v41";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
