a31a89dc174f727246ebdee82520836631c69b88
Deploy <d@e.co>
2026-07-13 22:01:10 +0000

    Devine le lieu (photo mystere dans les fils, propositions, revelation) + Quiz du sejour (questions generees, scores du groupe), 12 interrupteurs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v48";
+const CACHE = "vacances-v49";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
