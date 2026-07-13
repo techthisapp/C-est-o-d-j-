e2e15846475b32c65fa9e79b473ee8b0ab13c49e
Deploy <d@e.co>
2026-07-13 07:20:21 +0000

    Sondages: creation dans les fils, vote public multi/simple, cloture, bandeau accueil, pastille programme, creer l-activite

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v19";
+const CACHE = "vacances-v20";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
