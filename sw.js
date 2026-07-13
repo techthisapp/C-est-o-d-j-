e17340533281238e962e7e032cc39e517c236bc3
Deploy <d@e.co>
2026-07-13 20:20:43 +0000

    Photos partagees via Supabase Storage: envoi optimiste, synchro des liens, fusion par union, reactions sur photos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v39";
+const CACHE = "vacances-v40";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
