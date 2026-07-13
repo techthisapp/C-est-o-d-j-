694194648b8f7e7a8b4e39b42416726362d8a9d6
Deploy <d@e.co>
2026-07-13 07:05:11 +0000

    Pilules teintees par type, separateur, resserrees; pastille de nouveaux messages sur Discussion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v18";
+const CACHE = "vacances-v19";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
