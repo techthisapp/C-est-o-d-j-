3671438ca535af25d876070883eb7a03c354b7de
Deploy <d@e.co>
2026-07-13 15:23:00 +0000

    Categorie de base Fete (emoji fete, rose, adaptee au theme)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v35";
+const CACHE = "vacances-v36";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
