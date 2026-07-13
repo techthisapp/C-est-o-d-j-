d78d2a81ae8dbbaad1fd0c3fc08938125b5958b8
Deploy <d@e.co>
2026-07-13 07:42:44 +0000

    Date au creux de l-arc; animations douces: halo soleil, lune, etoiles, toucher, pop pastilles, fondu onglets, barre fluide

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v22";
+const CACHE = "vacances-v23";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
