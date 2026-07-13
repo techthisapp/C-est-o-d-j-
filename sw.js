ffc2f516fee957a963fe26cd031a8588d0ae76f1
Deploy <d@e.co>
2026-07-13 07:24:03 +0000

    Carte compacte PUIS A: actions rapides et bandeau sondage

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v20";
+const CACHE = "vacances-v21";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
