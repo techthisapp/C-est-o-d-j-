b1b53ab7311039db35314dd40bbde45fd2e7280f
Deploy <d@e.co>
2026-07-13 14:08:15 +0000

    Lieux favoris partages: enregistrer depuis le formulaire, reutiliser en puces, gerer dans les reglages

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v31";
+const CACHE = "vacances-v32";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
