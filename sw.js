7b8c657e2f9614f71b98fdef1e0b39997a9a2fde
Deploy <d@e.co>
2026-07-13 21:38:18 +0000

    Interactions du sejour: socle d-interrupteurs dans les reglages (partages) + statut en direct sur l-accueil (choix predefinis, expiration 3h)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v46";
+const CACHE = "vacances-v47";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
