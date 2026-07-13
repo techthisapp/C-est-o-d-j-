a446dae5bf3ecf483a5df3aaafd4b8a4ee067658
Deploy <d@e.co>
2026-07-13 04:35:54 +0000

    Bouton Modifier plus grand, suppression d-activite en rouge avec confirmation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v13";
+const CACHE = "vacances-v14";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
