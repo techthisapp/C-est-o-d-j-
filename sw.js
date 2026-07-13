4e7fbba7a8746ee61b279c69814560cdfa6acde7
Deploy <d@e.co>
2026-07-13 07:35:44 +0000

    Bouton + sur l-accueil: ajout rapide d-une activite au jour courant

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v21";
+const CACHE = "vacances-v22";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
