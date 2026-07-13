6132b346405a2627232c5e202be29c78b4089b85
Deploy <d@e.co>
2026-07-13 20:11:34 +0000

    Reactions emoji sur les messages (fiche activite et discussion generale): picker, pastilles compteur, bascule, fusion par personne en synchro

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v38";
+const CACHE = "vacances-v39";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
