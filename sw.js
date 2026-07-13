82a58911277582813fb866fffa293e9f27bd7422
Deploy <d@e.co>
2026-07-13 14:22:52 +0000

    Horaires: decalage auto fin=debut+1h si debut depasse fin, et debut=fin-1h si fin avant debut, restant editables

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v33";
+const CACHE = "vacances-v34";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
