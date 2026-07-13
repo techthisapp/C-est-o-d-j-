1c2cc611a8f29d7b513136859cfc33e5fedb7b9d
Deploy <d@e.co>
2026-07-13 17:45:25 +0000

    Pilule Discussion des cartes accueil ouvre directement au niveau du fil (defilement vers la conversation)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v36";
+const CACHE = "vacances-v37";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
