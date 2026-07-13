64f91ae21c522419f263b2e49ae65651ad2647ce
Deploy <d@e.co>
2026-07-13 06:15:31 +0000

    Programme: glisser vers la gauche pour supprimer une activite (avec confirmation)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v15";
+const CACHE = "vacances-v16";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
