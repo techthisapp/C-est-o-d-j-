4783eaed93d78c127e2cc9b304c9394caaabf34f
Deploy <d@e.co>
2026-07-13 14:18:49 +0000

    Recherche de lieu geolocalisee: biais sur la position, tri par distance, distance affichee, mondial

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v32";
+const CACHE = "vacances-v33";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
