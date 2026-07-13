0770566b3a4d36e847cf2a40744ac20536fcfbf7
Deploy <d@e.co>
2026-07-13 07:46:09 +0000

    Heure courante au creux de l-arc sous la date, horloge rafraichie toutes les 10 s

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v23";
+const CACHE = "vacances-v24";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
