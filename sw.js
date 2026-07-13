356ccedc8ada77f0fd87f152caa8215fa7b953db
Deploy <d@e.co>
2026-07-13 08:59:27 +0000

    Programme: bouton + en tete aligne comme l-accueil, retrait du bouton Ajouter en bas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v27";
+const CACHE = "vacances-v28";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
