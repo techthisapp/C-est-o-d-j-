c998df37fcf651a463d6209e10cf6d7ed7cb6347
Deploy <d@e.co>
2026-07-13 06:55:05 +0000

    Actions rapides sur cartes accueil (Itineraire Plans/Maps, Discussion, Photo), itineraire Plans dans la fiche

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v17";
+const CACHE = "vacances-v18";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
