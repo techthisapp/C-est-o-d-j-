1750822ed7f464ec260d9e5cd639372a5f1e310c
Deploy <d@e.co>
2026-07-13 20:50:09 +0000

    Groupe: boutons contact ronds alignes au nom (liste condensee) + garde-fou synchro (pas d-envoi avant lecture reussie de l-etat partage)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v43";
+const CACHE = "vacances-v44";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
