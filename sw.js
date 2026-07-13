3245fb7a125b42e32a2b284ac3e8276aebebe1d1
Deploy <d@e.co>
2026-07-13 18:00:52 +0000

    Fond evolutif selon l-heure: couleurs interpolees en continu (aube, jour, dore, couchant, crepuscule, nuit), clair et sombre, barre d-etat synchronisee

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v37";
+const CACHE = "vacances-v38";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
