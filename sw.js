308c9a14788825cd7225ed0ee7a4de8469b95967
Deploy <d@e.co>
2026-07-14 10:49:15 +0000

    Bande d-infos du jour sous l-arc: lever, meteo, vent, coucher en blocs alignes avec icones lucide colorees, valeurs Fredoka et libelles, separateurs fins; SVG de l-arc rendu a son etat epure

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v58";
+const CACHE = "vacances-v59";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
