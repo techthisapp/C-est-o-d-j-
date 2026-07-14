b837bf764a413e6f9183c7782569053743aa1f42
Deploy <d@e.co>
2026-07-14 12:25:55 +0000

    Arc solaire redessine: ellipse pleine largeur, trainee doree de la course parcourue, halo radial autour du soleil, heure agrandie, etoiles la nuit, ligne d-infos grise integree sous l-horizon sans libelles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v60";
+const CACHE = "vacances-v61";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
