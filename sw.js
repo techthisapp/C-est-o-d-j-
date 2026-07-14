6e8ea155926362fd037bb04294a8aa3250dd97a5
Deploy <d@e.co>
2026-07-14 10:28:54 +0000

    Arc solaire: lever/coucher reels du lieu aux pieds de l-arc (calcul astronomique local), meteo Open-Meteo discrete (temperature + icone) et donnee selon le type de voyage (vent en mer, pluie en rando, ressenti sinon), cache 45 min, degradation propre, normalisation place

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v56";
+const CACHE = "vacances-v57";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
