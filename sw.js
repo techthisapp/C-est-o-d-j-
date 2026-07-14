ccdef1077c67d054792b42af6dc909120e716678
Deploy <d@e.co>
2026-07-14 07:39:11 +0000

    Reaction rapide: fontaine d-emojis au tap, compteur d-ambiance partage sur la carte (message unique agrege par activite, fusion max par personne), ligne discrete dans le fil, exclue des non-lus

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v52";
+const CACHE = "vacances-v53";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
