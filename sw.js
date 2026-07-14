0e0f6d088c3af41838ee161ff5bc5215cba2274a
Deploy <d@e.co>
2026-07-14 10:52:21 +0000

    Infos du jour dans les flancs de l-arc (meteo et lever a gauche, vent et coucher a droite), superposees sans hauteur ajoutee

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v59";
+const CACHE = "vacances-v60";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
