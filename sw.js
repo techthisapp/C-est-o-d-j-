0e8caae57ff78f0004c88120f4bd184655f9bbc3
Deploy <d@e.co>
2026-07-14 07:33:12 +0000

    Jeux: retrait de la tuile Film (doublon), le film reste accessible depuis le bouton lecture du mur photo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v51";
+const CACHE = "vacances-v52";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
