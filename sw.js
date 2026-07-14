a91b4a78df1ccd84805f6d1c25a8f3a27ea970cc
Deploy <d@e.co>
2026-07-14 09:28:10 +0000

    Contenu 14 jours sans repetition: 16 questions du matin, 16 votes, 15 themes photo par type de voyage (96 questions, 96 votes, 90 themes au total, zero doublon), garanti aussi en ambiance famille, extras minuit enrichis

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vacances-v55";
+const CACHE = "vacances-v56";
 self.addEventListener("install", (e) => { self.skipWaiting(); });
 self.addEventListener("activate", (e) => { self.clients.claim(); });
 
